9b0ea2c0b8ad575d426bfabb2c2685b749ab15c7
layerunk <89783661+layerunk@users.noreply.github.com>
2023-03-06 18:16:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,49 @@
 function setup(){
-    canvas = createCanvas(600 , 300);
+    canvas = createCanvas(600 , 400);
     canvas.position(450, 300);
     background("white");
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function preload(){
+    son1 = loadSound("music.mp3");
+    son2 = loadSound("music2.mp3");
+    son3 = loadSound("music3.mp3");
+    son = 0;
+}
+
+function draw(){
+    image(video, 0, 0, 600, 400);
+}
+
+function play12(){
+    son1.play();
+}
+
+function next12(){
+        son = son + 1;
+        son1.stop();
+        son2.stop();
+        son3.stop();
+
+        if(son > 3){
+            son = 1
+        }
+        if(son == 1){
+            son1.play();
+        }
+        if(son == 2){
+            son2.play();
+        }
+        if(son == 3){
+            son3.play();
+        }
+}
+
+function stop12(){
+    son1.stop();
+    son2.stop();
+    son3.stop();
 }
